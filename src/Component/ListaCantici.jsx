cd60f29ef3fd91db1e277e1534ae55f9949e76eb
Camillo Fallù <camillofallu9920@gmail.com>
2025-04-01 17:13:50 +0200

    modifiche

--- src/Component/ListaCantici.jsx
+++ src/Component/ListaCantici.jsx
@@ -379,7 +379,7 @@ export default function ListaCantici() {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
-                fill="red"
+                fill="maroon"
                 className="size-6"
               >
                 <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
@@ -416,7 +416,7 @@ export default function ListaCantici() {
                     viewBox="0 0 24 24"
                     fill={
                       preferiti.some((item) => item.id === cantico.id)
-                        ? "red"
+                        ? "maroon"
                         : "gray"
                     }
                     className="size-6"
